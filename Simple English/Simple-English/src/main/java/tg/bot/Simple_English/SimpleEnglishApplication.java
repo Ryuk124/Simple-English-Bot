57bdc4c9de1c23450b6d90a98bfb4fd381c7c220
Ryuk124 <60621197+Ryuk124@users.noreply.github.com>
2023-11-06 23:12:34 +0500

    Initial commit

---
+++ Simple English/Simple-English/src/main/java/tg/bot/Simple_English/SimpleEnglishApplication.java
@@ -0,0 +1,13 @@
+package tg.bot.Simple_English;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEnglishApplication.class, args);
+	}
+
+}
---
+++ Simple English/Simple-English/src/test/java/tg/bot/Simple_English/SimpleEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.bot.Simple_English;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEnglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
