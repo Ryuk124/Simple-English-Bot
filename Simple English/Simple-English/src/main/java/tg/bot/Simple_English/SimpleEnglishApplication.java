2becd56a55d27cf86c48aa932fa91b83916da420
Ryuk124 <60621197+Ryuk124@users.noreply.github.com>
2023-11-07 00:55:46 +0500

    Fixed some bugs

--- Simple English/Simple-English/src/main/java/tg/bot/Simple_English/SimpleEnglishApplication.java
+++ Simple English/Simple-English/src/main/java/tg/bot/Simple_English/SimpleEnglishApplication.java
@@ -2,8 +2,9 @@ package tg.bot.Simple_English;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SimpleEnglishApplication {
 
 	public static void main(String[] args) {
